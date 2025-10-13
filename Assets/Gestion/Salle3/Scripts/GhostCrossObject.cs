dcf7da7f93a4f2679f918b5a0cdb24557f11dca3
kaciafr <kaciait581@gmail.com>
2025-10-13 10:37:55 +0200

    Rebase vivi

---
+++ Assets/Gestion/Salle3/Scripts/GhostCrossObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GhostCroosObject : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask triggerObject;
+
+    public int gaugeMax;
+    
+    public int currentGauge;
+
+    public bool;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
