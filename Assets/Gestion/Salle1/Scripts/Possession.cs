19fb0e99d8895da826e605b9a71f36cac564f407
Vivien <viviane-rohaut@outlook.fr>
2025-10-13 12:00:32 +0200

    Possession
    debut du code tkt

---
+++ Assets/Gestion/Salle1/Scripts/Possession.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Possession : MonoBehaviour
+{
+    public Transform player;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
