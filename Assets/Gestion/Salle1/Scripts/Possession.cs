227348e5c4225d5dd7df63cd6d82229fa518192e
Vivien <viviane-rohaut@outlook.fr>
2025-10-18 21:12:21 +0200

    Mécanique Possession
    base (changement de personnage contrôlable et retour à la normale)

--- Assets/Gestion/Salle1/Scripts/Possession.cs
+++ Assets/Gestion/Salle1/Scripts/Possession.cs
@@ -1,16 +1,44 @@
+using Unity.Hierarchy;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Possession : MonoBehaviour
 {
-    public Transform player;
     
-    void Start()
-    {
-        
-    }
+    public GameObject ghost;
+    public Camera ghostCamera;
+    public PlayerMovement playerMovement;
     
+    public GameObject hamster;
+    public Camera hamsterCamera;
+    public HamsterController hamsterController;
+    
+    private KeyCode toggleKey = KeyCode.E;
+
     void Update()
     {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            Switch();
+        }
+    }
+
+    void Switch()
+    {
+        if (ghost == null || hamster == null)
+        {
+            return;
+        }
+
+        bool aActive = ghost.activeSelf;
+
+        ghost.SetActive(!aActive);
+        ghostCamera.enabled = !aActive;
+        playerMovement.enabled = !aActive;
         
+        hamster.SetActive(aActive);
+        hamsterCamera.enabled = aActive;
+        hamsterController.enabled = aActive;
     }
 }
+
